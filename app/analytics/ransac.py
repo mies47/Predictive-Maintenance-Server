82a60e2a8087bf1e0ddc89755a2b6dbb451b984a
Arian <aryan23@aut.ac.ir>
2023-03-28 22:50:39 +0330

    update: RANSAC algorithm

--- app/analytics/ransac.py
+++ app/analytics/ransac.py
@@ -1,4 +1,12 @@
+from sklearn.linear_model import RANSACRegressor
+
 
 class RANSAC:
     def __init__(self):
-        pass
+        self.model = RANSACRegressor()
+
+    def train(self, train_data = None, train_data_prediction = None):
+        if train_data is None:
+            return
+        
+        self.model.fit(train_data, train_data_prediction)
