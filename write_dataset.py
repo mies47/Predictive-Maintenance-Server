044016a7f30eab9daf9ef558b964d62920d8a198
2arian3 <aryan.boukani2@gmail.com>
2023-06-20 23:27:07 +0330

    update: write dataset script according to the new verification system

--- write_dataset.py
+++ write_dataset.py
@@ -3,111 +3,73 @@ import json
 import time
 import tqdm
 import uuid
-import requests
 import random
 import pandas as pd
-from copy import deepcopy
 
-from typing import List, Dict
 from dotenv import load_dotenv
+from datetime import datetime
 
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 load_dotenv()
 
 
-BASE_URL = f'http://{os.getenv("SERVER_HOST")}:{os.getenv("SERVER_PORT")}{os.getenv("API_PREFIX")}'
-
 DATASET_MACHINES = [uuid.uuid4().hex for _ in range(4)]
 DATASET_MEASUREMENTS = [uuid.uuid4().hex for _ in range(10)]
 
+INFLUXDB_HOST = os.getenv('INFLUXDB_HOST')
+INFLUXDB_PORT = os.getenv('INFLUXDB_PORT')
+INFLUXDB_ORG = os.getenv('INFLUXDB_ORG')
+INFLUXDB_BUCKET = os.getenv('INFLUXDB_BUCKET')
+INFLUXDB_TOKEN = os.getenv('INFLUXDB_TOKEN')
 
-class ModelJsonObject(json.JSONEncoder):
-	def default(self, obj):
-		if hasattr(obj, "__dict__"):
-			return {key:value for key, value in obj.__dict__.items() if not key.startswith("_")}
-		return super().default(obj)
-
-
-class VibrationData:
-	def __init__(self, x: float, y: float, z: float):
-		self.x = x
-		self.y = y
-		self.z = z
-	
-	def __repr__(self) -> str:
-		return f"x: {self.x}, y: {self.y}, z: {self.z}"
-
-
-class Measurement:
-	def __init__(self, id: str, time: float, data: List[VibrationData] = []):
-		self.time = deepcopy(time)
-		self.id = deepcopy(id)
-		self.data = deepcopy(data)
-
-	def add_new_data(self, new_data: VibrationData):
-		self.data.append(new_data)
-	
-	def add_new_data_list(self, new_data_list: List[VibrationData]):
-		self.data.extend(new_data_list)
-
-
-class Node:
-	def __init__(self, node_id:str, measurements: Dict[str, Measurement] = {}):
-		self.node_id = node_id
-		self.measurements = deepcopy(measurements)
-
-	def add_measurement(
-		self,
-		id: str,
-		time: float,
-		data: List[VibrationData]
-	):
-		'''
-		If this is a new measurement add it to node and start the timer to check
-		sampling, if not, add it to the previously added measurement.
-		'''
-		if not self.measurements.get(id):
-			self.measurements[id] = Measurement(id, time, data)
-		else:
-			self.measurements[id].add_new_data_list(data)
+INFLUXDB_URI = f'http://{INFLUXDB_HOST}:{INFLUXDB_PORT}'.replace(' ', '%20')
 
 
 if __name__ == '__main__':
-	df = pd.read_csv('./datasets/accelerometer.csv', usecols=['x', 'y', 'z'])
-	datasetLength = 10000
-	machinesLength = len(DATASET_MACHINES)
-	measurementLength = len(DATASET_MEASUREMENTS)
-
-	t1 = int(time.time() - 20 * 60 * 60 * 24)
-	t2 = int(time.time())
-	
-	dList = {}
-
-	for i, row in tqdm.tqdm(df.iterrows()):
-		if i == datasetLength - 1:
-			break
-
-		machineIndex = int((i / datasetLength) * machinesLength)
-		measurementIndex = int(((i % (datasetLength // machinesLength)) / (datasetLength // machinesLength)) * measurementLength)
-
-		vData = VibrationData(x=row['x'], y=row['y'], z=row['z'])
-
-		node_id = DATASET_MACHINES[machineIndex]
-		measurement_id = DATASET_MEASUREMENTS[measurementIndex]
-  
-		if node_id in dList:
-			dList[node_id].add_measurement(id=measurement_id, time=random.randint(t1, t2), data=[vData])
-		else:
-			dList[node_id] = Node(node_id=node_id)
-			dList[node_id].add_measurement(id=measurement_id, time=random.randint(t1, t2), data=[vData])
-
-	r = requests.post(f'{BASE_URL}/signup/gateway', data=json.dumps({'mac': uuid.uuid4().hex, 'password': 'test'}))
-	token = r.json().get('token')
-	r.close()
-
-	auth = {'Authorization': f'Bearer {token}'}
-
-	r = requests.post(f'{BASE_URL}/data', data=json.dumps(dList, cls=ModelJsonObject, indent=4), headers=auth)
-	print(f'STATUS: {r.status_code}')
-	r.close()
+    df = pd.read_csv('./datasets/accelerometer.csv', usecols=['x', 'y', 'z'])
+    datasetLength = 10000
+    
+    machinesLength = len(DATASET_MACHINES)
+    measurementLength = len(DATASET_MEASUREMENTS)
+    
+    t1 = int(time.time() - 20 * 60 * 60 * 24)
+    t2 = int(time.time())
+    
+    points = []
+    
+    print('***READING DATASET***')
+    
+    for i, row in tqdm.tqdm(df.iterrows()):
+        if i == datasetLength - 1:
+            break
+        
+        machineIndex = int((i / datasetLength) * machinesLength)
+        measurementIndex = int(((i % (datasetLength // machinesLength)) / (datasetLength // machinesLength)) * measurementLength)
+        
+        nodeId = DATASET_MACHINES[machineIndex]
+        measurementId = DATASET_MEASUREMENTS[measurementIndex]
+        
+        new_point = Point('vibration_measurement')\
+                        .tag('nodeId', nodeId)\
+                            .tag('measurementId', measurementId)\
+                                .tag('index', i)\
+                                    .field('x', row['x'])\
+                                        .field('y', row['y'])\
+                                            .field('z', row['z'])\
+                                                .time(time=datetime.utcfromtimestamp(random.randint(t1, t2)))
+                                                
+        points.append(new_point)
+        
+    print('***CONNECTING TO DATABASE***')
+    
+    client = InfluxDBClient(url=INFLUXDB_URI, org=INFLUXDB_ORG, token=INFLUXDB_TOKEN)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    
+    print('***WRITING DATASET***')
+    
+    write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=points)
+    
+    print('DONE')
